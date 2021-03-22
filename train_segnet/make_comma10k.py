7c6dc961ed6b2fc7baf939ce7498d521bcd6ce72
Zeke Hobbs <zshobbs@gmail.com>
2021-03-22 01:27:24 +0000

    added MP Pool

--- train_segnet/make_comma10k.py
+++ train_segnet/make_comma10k.py
@@ -3,24 +3,23 @@ import cv2
 import glob
 import numpy as np
 from tqdm import tqdm
+from multiprocessing import Pool
 
 # Save location for catagorical masks and current mask dir
 cat_mask_save_dir = os.path.join('comma10k', 'cat_masks')
 mask_dir = os.path.join('comma10k', 'masks')
 
-m_split = "\\" if os.name == "nt" else "\\"
+m_split = "\\" if os.name == "nt" else "/"
 
-def mask2cat(mask_paths):
-    print('Convering masks')
-    for m in tqdm(mask_paths):
-        # Read in images greyscale
-        mask = cv2.imread(m, 0)
-        unique_vales = np.unique(mask)
-        # Only works for current unique values
-        for cat_num, class_num in enumerate(unique_vales):
-            mask[mask==class_num] = cat_num
+def mask2cat(m):
+    # Read in images greyscale
+    mask = cv2.imread(m, 0)
+    unique_vales = np.unique(mask)
+    # Only works for current unique values
+    for cat_num, class_num in enumerate(unique_vales):
+        mask[mask==class_num] = cat_num
 
-        cv2.imwrite(os.path.join(cat_mask_save_dir, m.split(m_split)[-1]), mask)
+    cv2.imwrite(os.path.join(cat_mask_save_dir, m.split(m_split)[-1]), mask)
 
 if __name__ == "__main__":
     if not os.path.exists(cat_mask_save_dir):
@@ -28,4 +27,9 @@ if __name__ == "__main__":
 
     # Get all mask paths
     mask_file_paths = glob.glob(os.path.join(mask_dir, '*.png'))
-    mask2cat(mask_file_paths)
+    p = Pool(os.cpu_count())
+    for _ in tqdm(p.imap_unordered(mask2cat, mask_file_paths), total=len(mask_file_paths)):
+        pass
+
+    p.close()
+    p.join()
